bd93b632fd7badcf138e8780f9e8a8b9ea7539bb
Raii <raisanuraida11@gmail.com>
2024-09-27 11:14:35 +0700

    second commit

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,3 +6,5 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
+$routes->get('/import', 'ImportController::index');
+$routes->post('/import/import', 'ImportController::import');
--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -2,6 +2,13 @@
 
 namespace App\Controllers;
 
+use App\Models\dashboard;
+use App\Models\UserModel;
+use CodeIgniter\Controller;
+use PhpOffice\PhpSpreadsheet\Spreadsheet;
+use PhpOffice\PhpSpreadsheet\Reader\Xlsx;
+use SebastianBergmann\CodeCoverage\Report\Html\Dashboard as HtmlDashboard;
+
 class Home extends BaseController
 {
     public function index(): string
@@ -9,3 +16,4 @@ class Home extends BaseController
         return view('index');
     }
 }
+
